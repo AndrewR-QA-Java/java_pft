c2ef14748a9facd6bcec291f5417e21897dd6660
AndrewR <hix@o2.pl>
2020-01-09 18:13:27 +0100

    Lesson 4.10

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,19 +3,10 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  private int id;
-
-  public int getId() {
-    return id;
-  }
 
   public GroupData(int id, String name, String header, String footer) {
     this.id = id;
@@ -25,10 +16,18 @@ public class GroupData {
   }
 
   public GroupData(String name, String header, String footer) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.name = name;
     this.header = header;
     this.footer = footer;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getId() {
+    return id;
   }
 
   public String getName() {
@@ -44,24 +43,23 @@ public class GroupData {
   }
 
   @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return id == groupData.id &&
-            Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(name, id);
-  }
-
-  @Override
   public String toString() {
     return "GroupData{" +
             "name='" + name + '\'' +
             ", id='" + id + '\'' +
             '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
   }
 }
