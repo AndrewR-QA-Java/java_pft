0e0141ae5b45bf23522a7effe6c00c0ca73e8c67
AndrewR <hix@o2.pl>
2020-03-16 22:45:35 +0100

    Revert "Lesson 4.9"
    This reverts commit 9e7c702ecf99d0fb5fb24cff84cd962ce98e456b.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -13,7 +13,7 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test2", null, null);
+    GroupData group = new GroupData("test1", null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
@@ -24,7 +24,7 @@ public class GroupCreationTests extends TestBase {
         max = g.getId();
       }
     }
-    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+    group.setId(max);
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
