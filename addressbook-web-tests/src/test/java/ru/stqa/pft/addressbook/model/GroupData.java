fe765f3e1f99ab0113bdb3d2ffe4962b94d85fb3
AndrewR <hix@o2.pl>
2020-03-16 22:45:31 +0100

    Revert "Lesson 4.10"
    This reverts commit c2ef14748a9facd6bcec291f5417e21897dd6660.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,10 +3,19 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-  private int id;
   private final String name;
   private final String header;
   private final String footer;
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  private int id;
+
+  public int getId() {
+    return id;
+  }
 
   public GroupData(int id, String name, String header, String footer) {
     this.id = id;
@@ -16,18 +25,10 @@ public class GroupData {
   }
 
   public GroupData(String name, String header, String footer) {
-    this.id = Integer.MAX_VALUE;
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  public int getId() {
-    return id;
   }
 
   public String getName() {
@@ -43,23 +44,24 @@ public class GroupData {
   }
 
   @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return id == groupData.id &&
+            Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, id);
+  }
+
+  @Override
   public String toString() {
     return "GroupData{" +
             "name='" + name + '\'' +
             ", id='" + id + '\'' +
             '}';
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(name);
   }
 }
