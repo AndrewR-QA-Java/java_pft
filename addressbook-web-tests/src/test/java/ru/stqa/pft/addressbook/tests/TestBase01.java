e93ebfcd2cfb849b2bd96a067979206d93a120bf
AndrewR <hix@o2.pl>
2020-03-17 10:38:10 +0100

    Revert "Tests for Edge/FF/Chrome"
    This reverts commit 04a5568a909d88124e6b066024528b6736cd201c.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase01.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase01.java
@@ -3,20 +3,19 @@ package ru.stqa.pft.addressbook.tests;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import ru.stqa.pft.addressbook.appmanager.ApplicationManager01;
 
-public class TestBase {
+public class TestBase01 {
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected final ApplicationManager01 app = new ApplicationManager01(BrowserType.IE);
 
   @BeforeMethod
   public void setUp() throws Exception {
     app.init();
   }
 
-//  @AfterMethod
-//  public void tearDown() {
-//    app.stop();
-//  }
-
+  @AfterMethod
+  public void tearDown() {
+    app.stop();
+  }
 }
