6c492402a613d125fe29e55f755d6b6495274754
AndrewR <54781249+AndrewR-QA-Java@users.noreply.github.com>
2021-01-03 19:23:18 +0100

    Lesson 8.2 A new module was added for Mantis tests

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+public class TestBase {
+
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+
+  @BeforeSuite
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() {
+    app.stop();
+  }
+}
